c3ec1db7146c121a955aa5cd9226dc04933b381c
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-05-10 16:46:02 +0530

    feat : added route with PUT request to update a resume by email

--- server/src/routes/resumeRoutes.ts
+++ server/src/routes/resumeRoutes.ts
@@ -1,10 +1,15 @@
 import express from 'express';
-import { addResume, getResume,deleteResume } from '../controllers/resumeController';
+import { addResume, getResume,deleteResume,updateResume } from '../controllers/resumeController';
 
 const router = express.Router();
 
+// POST request to add a new resume
 router.post('/add', addResume);
+// GET request to fetch a resume by email
 router.get('/:email', getResume);
+// PUT request to update a resume by email
+router.put('/:email', updateResume);
+// DELETE request to delete a resume by email
 router.delete('/:email', deleteResume);
 
 
