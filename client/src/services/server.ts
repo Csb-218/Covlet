f2fe88934ddbd90ed8d8831be9c48c46c3313805
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-05-10 16:47:10 +0530

    feat : added resume update service

--- client/src/services/server.ts
+++ client/src/services/server.ts
@@ -44,6 +44,31 @@ export async function addResumeDataToDB(resume: IProfileSchema) {
     }
 }
 
+// API endpoint to update resume data in the database
+export async function updateResumeDataInDB(resume: IProfileSchema, email: string) {
+    // Check if the resume object is empty
+    if (Object.keys(resume).length === 0) {
+        console.error("Resume object is empty");
+        throw new Error("Resume object is empty.");
+    }
+    try {
+        const options = {
+            url : `/resume/${email}`,
+            method: "PUT",
+            baseURL: import.meta.env.WXT_SERVER_URL,
+            data: resume
+        }
+        const response = await axios.request(options)
+        if (response.status !== 201) {
+            throw new Error("Failed to update resume data");
+        }
+        return response.data.data
+    } catch (error) {
+        console.error("Error updating resume data in DB:", error);
+        throw new Error("Failed to update resume data in DB.");
+    }
+}
+
 // API endpoint to get resume data from the database
 export async function getResumeDataFromDB(email: string) {
     try {
@@ -62,7 +87,7 @@ export async function getResumeDataFromDB(email: string) {
             throw new Error("Failed to fetch resume data");
         }
 
-        return response.data as IProfileSchema;
+        return response.data.data as IProfileSchema;
         
     } catch (error) {
         console.error("Error getting resume data from DB:", error);
