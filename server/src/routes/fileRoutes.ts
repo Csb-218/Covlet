e7ed8febd1a9483356f3ae37df819558502f188e
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-05-14 21:19:23 +0530

    feat : added file upload middleware at router

--- server/src/routes/fileRoutes.ts
+++ server/src/routes/fileRoutes.ts
@@ -1,10 +1,16 @@
-import express from 'express';
-import multer from 'multer';
+import { Router } from 'express';
+import upload from '../middlewares/fileUpload'
 import { fileReadController } from '../controllers/fileReadController';
 
-const router = express.Router();
-const upload = multer({ dest: 'uploads/' });
-
-router.post('/', upload.single('file'), fileReadController);
+const router = Router();
+router.post('/', function(req,res,next){
+  upload(req,res,function(err){
+    if(err){
+      console.log(err)
+      return res.status(400).json({error:err.message})
+    }
+    next()
+  })
+}, fileReadController);
 
 export default router;
