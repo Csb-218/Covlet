25f09808d169772ae2711f4902aa4f8bb8281172
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-05-14 21:15:19 +0530

    feat : added edge case(test if resume contains specific keywords)
    • added edge case(test if resume contains specific keywords)
    • code refactorization

--- server/src/controllers/fileReadController.ts
+++ server/src/controllers/fileReadController.ts
@@ -1,42 +1,51 @@
 import fs from 'fs';
 import { Request, Response } from 'express';
 import { returnResumeFilledSchema} from "../services/mistral_service"
-const pdf = require('pdf-parse');
+import PdfParse,{Result} from "pdf-parse"
 
 export const fileReadController = async (req: Request, res: Response) => {
   try {
+    // Check if the file is present in the request
     if (!req.file) {
       return res.status(400).json({ error: 'No file uploaded' });
     }
 
     // Read file as buffer instead of utf-8 string
-    const buffer = fs.readFileSync(req.file.path);
-    
-    // Convert buffer to Blob
-    // const blob = new Blob([buffer], { type: req.file.mimetype });
+    const buffer:Buffer = fs.readFileSync(req.file.path);
 
-    const data = await pdf(buffer);
+    // extract data from the PDF
+    const data:Result = await PdfParse(buffer);
+
+    // Use pdf-parse to extract text from the PDF
+    const pdfText:string = data.text;
+
+    // Check if the data contains the expected texts using RegExp.test()
+    const educationRegex = /\b(education)\b/i;
+    const experienceRegex = /\b(experience)\b/i;
     
+    if (!educationRegex.test(data.text) || !experienceRegex.test(data.text)) {
+      return res.status(400).json({ error: 'The resume does not have education and experience' });
+    }
+
     // Log PDF information
     console.log('Number of pages:', data.numpages);
     console.log('Number of rendered pages:', data.numrender);
     console.log('PDF info:', data.info);
     console.log('PDF metadata:', data.metadata);
     console.log('PDF.js version:', data.version);
-    // console.log('PDF text:', data.text);
 
     // Clean up the temporary file
     await fs.promises.unlink(req.file.path);
 
-    const schemaResponse = await returnResumeFilledSchema(data.text)
+    // get the schema from the PDF text
+    const schemaResponse = await returnResumeFilledSchema(pdfText)
 
     return res.status(200).json({
-      email: req.body.email,
       ...schemaResponse
     });
     
   } catch (error) {
     console.error('Error reading file:', error);
-    res.status(500).json({ error: 'Error reading file' });
+    res.status(500).json({ error: "Error reading file" });
   }
 };
