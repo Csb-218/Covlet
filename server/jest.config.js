d2d877a415abf1420a5907e821bfb0252f1982c2
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-05-15 19:05:09 +0530

    feat : added test coverage

--- server/jest.config.js
+++ server/jest.config.js
@@ -4,4 +4,13 @@ module.exports = {
   transform: {
     "^.+\.tsx?$": ["ts-jest",{}],
   },
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "html"],
+  collectCoverageFrom: [
+    "src/**/*.{js,ts}",
+    "!src/index.ts",
+    "!src/config/*.ts",
+    "!**/node_modules/**"
+  ]
 };
