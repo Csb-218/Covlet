415a73d78b9bbc415e312d6a25b96a72076bd481
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-09-21 01:12:54 +0530

    update : resume model field

--- server/src/models/ResumeModel.ts
+++ server/src/models/ResumeModel.ts
@@ -6,39 +6,39 @@ interface IResume extends Document, IProfileSchema {}
 
 const ResumeSchema = new Schema<IResume>({
   personal: {
-    name: { type: String, required: true },
-    email: { type: String, required: true , unique: true },
-    phone: { type: String, required: true },
-    linkedin: { type: String, required: true }
+    name: { type: String , required: true },
+    email: { type: String ,required: true , unique: true },
+    phone: { type: String},
+    linkedin: { type: String }
   },
-  summary: { type: String, required: true },
+  summary: { type: String },
   experience: [{
-    title: { type: String, required: true },
-    company: { type: String, required: true },
-    startDate: { type: Date, required: true },
-    endDate: { type: Date, required: true },
-    responsibilities: { type: String, required: true },
-    achievements: { type: String, required: true }
+    title: { type: String },
+    company: { type: String },
+    startDate: { type: Date },
+    endDate: { type: Date },
+    responsibilities: { type: String },
+    achievements: { type: String }
   }],
   education: [{
-    degree: { type: String, required: true },
-    institution: { type: String, required: true },
-    startDate: { type: Date, required: true },
-    endDate: { type: Date, required: true },
-    coursework: { type: String, required: true }
+    degree: { type: String },
+    institution: { type: String },
+    startDate: { type: Date },
+    endDate: { type: Date },
+    coursework: { type: String }
   }],
   skills: {
-    technical: [{ type: String ,required: true}],
-    soft: [{ type: String ,required: true}]
+    technical: [{ type: String}],
+    soft: [{ type: String}]
   },
   certifications: [{
-    name: { type: String, required: true },
-    year: { type: String, required: true }
+    name: { type: String },
+    year: { type: String }
   }],
   languages: [{ type: String }],
   projects: [{
-    name: { type: String, required: true, minlength: 2 },
-    description: { type: String, required: true, minlength: 10 },
+    name: { type: String, minlength: 2 },
+    description: { type: String, minlength: 10 },
     link: { type: String, required: false }
   }]
 }, {
