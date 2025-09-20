80c8d9021c1ad682106442c3acfd823263bb5686
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-09-21 01:09:31 +0530

    fix : sign in

--- client/src/entrypoints/popup/App.tsx
+++ client/src/entrypoints/popup/App.tsx
@@ -1,36 +1,61 @@
-import { useState, useEffect } from 'react';
-import { MemoryRouter, Routes, Route  } from "react-router-dom";
-import { user } from "../../types";
+import { useState, useEffect } from "react";
+import { MemoryRouter, Routes, Route } from "react-router-dom";
+import { IProfileSchema, user } from "../../types";
 import Home from "../../components/popup/Home";
-import ProfileBuild from '@/components/popup/ProfileBuild';
-import Layout from '../../components/popup/Layout';
-import Landing from '../../components/popup/Landing';
-import './App.css';
+import ProfileBuild from "@/components/popup/ProfileBuild";
+import Layout from "../../components/popup/Layout";
+import Landing from "../../components/popup/Landing";
+import { addResumeDataToDB } from "@/services/server";
+import "./App.css";
+
 
 function App() {
-  
-   const [user, setUser] = useState<user | null>(null);
-   const [isLoggingState, setLoggingState] = useState<boolean>(false);
+  const [user, setUser] = useState<user | null>(null);
+  const [isLoggingState, setLoggingState] = useState<boolean>(false);
 
-   const handleLogin = async () => {
-    try{
-      setLoggingState(true)
-      const loggedin = await chrome.runtime.sendMessage({ type: 'GOOGLE_LOGIN' });
-    }catch(error){
-      console.log(error)
-    }finally{
-      setLoggingState(false)
+  const handleLogin = async () => {
+    try {
+      setLoggingState(true);
+      const loggedin = await chrome.runtime.sendMessage({
+        type: "GOOGLE_LOGIN",
+      });
+      console.log(loggedin);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      // addResumeDataToDB(resume,user?.email)
+      setLoggingState(false);
     }
   };
 
   useEffect(() => {
     // Check if user is already logged in
-    chrome.storage.local.get(['user'], (result) => {
+    chrome.storage.local.get(["user"], (result) => {
       if (result.user) {
         setUser(result.user);
+        const resume:IProfileSchema = {
+          personal: {
+            name: result.user.name,
+            phone: "",
+            linkedin: "",
+            email: result.user.email,
+          },
+          summary: "",
+          experience: [],
+          education: [],
+          skills: {
+            technical: [],
+            soft: [],
+          },
+          certifications: [],
+          languages: [],
+          projects: [],
+        };
+        addResumeDataToDB(resume);
+        console.log(result.user.email);
       }
     });
-  }, [user,isLoggingState]);
+  }, [isLoggingState]);
 
   if (!user) {
     return (
@@ -42,16 +67,13 @@ function App() {
 
   return (
     <div className="h-full flex flex-col">
-      <MemoryRouter >
+      <MemoryRouter>
         <Layout>
           <Routes>
-            <Route
-              path="/"
-              element={<Home setUser={setUser} user={user} />}
-            />
+            <Route path="/" element={<Home setUser={setUser} user={user} />} />
             <Route
               path="/profile"
-              element={<ProfileBuild setUser={setUser} user={user}/>}
+              element={<ProfileBuild setUser={setUser} user={user} />}
             />
           </Routes>
         </Layout>
